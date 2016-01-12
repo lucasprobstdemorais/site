cd8dba76ff4cc0984c44a238f5b4792f8e17df70
diogogoebel <diogo.goebel@gmail.com>
2016-01-12 10:22:54 -0200

    hostname

--- ondeestou.php
+++ ondeestou.php
@@ -1,3 +1,4 @@
 <?php
-   echo "Estou no POD de IP: " . getenv("SERVER_ADDR") . "\n";
+ // echo "Estou no POD de IP: " . getenv("SERVER_ADDR") . "\n"; 
+  echo "Estou no POD de IP: " . getenv("HOSTNAME") . "\n";
 ?>
