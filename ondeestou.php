e2d63c50e731974bf68c40933256561cfccef674
diogogoebel <diogo.goebel@gmail.com>
2016-02-03 15:44:04 -0200

    pegando endereco IP

--- ondeestou.php
+++ ondeestou.php
@@ -1,4 +1,4 @@
 <?php
-//echo "Estou no POD de IP: " . getenv("SERVER_ADDR") . "\n"; 
-echo "Estou no POD: " . getenv("HOSTNAME") . "\n";
+echo "Estou no POD de IP: " . getenv("SERVER_ADDR") . "\n"; 
+//echo "Estou no POD: " . getenv("HOSTNAME") . "\n";
 ?>
